717fee328c4f4c53eef7655a613379863324abd1
Niko_Inas <niko.inas@gmail.com>
2024-01-10 21:50:29 +0400

    first test commit

--- Package.swift
+++ Package.swift
@@ -5,19 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "NEWKSPlayer",
+    platforms: [.macOS(.v10_15),
+                .iOS(.v13),
+                .tvOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NEWKSPlayer",
             targets: ["NEWKSPlayer"]),
     ],
+    dependencies: [.package(url: "https://github.com/kingslay/FFmpegKit.git", from: Version("6.1.0"))],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NEWKSPlayer"),
-        .testTarget(
-            name: "NEWKSPlayerTests",
-            dependencies: ["NEWKSPlayer"]),
+            name: "NEWKSPlayer",
+            dependencies: ["FFmpegKit"]
+        ),
     ]
 )
